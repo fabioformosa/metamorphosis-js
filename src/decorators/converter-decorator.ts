52e2afa25d4fd265a1701319c0773b3dd362837c
fabio.formosa <fabio.formosa@gmail.com>
2019-10-28 23:44:49 +0100

    refactored wrapped constructor

--- src/decorators/converter-decorator.ts
+++ src/decorators/converter-decorator.ts
@@ -1,22 +1,35 @@
 import { Converter } from '../model/converter';
 import ConverterRegistry from '../model/converter-registry';
+import { logger } from 'src/consts/log';
 
 const converterDecorator = (sourceClass: NewableFunction, targetClass: NewableFunction) => {
   return function<T extends {new(...args:any[]):{}}>(ConverterConstructor : T){
-    console.log(`Found converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
+    logger.log(`Found converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
 
-    const wrappedConverterConstructor: any = function (...args: any) {
-        // console.log(`New: ${ConverterConstructor['name']} is created`);
-        let converterInstance = new ConverterConstructor(...args);
-        converterRegistry.register( <Converter<NewableFunction, NewableFunction>> converterInstance, sourceClass, targetClass);
-        return converterInstance;
+    return class extends ConverterConstructor{
+      constructor(...args: any[]){
+        super(...args);
+        converterRegistry.register( <Converter<NewableFunction, NewableFunction>> <unknown>this, sourceClass, targetClass);
+        logger.log(`Registered new converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
+      }
     }
-    wrappedConverterConstructor.prototype = ConverterConstructor.prototype;
-
-    return wrappedConverterConstructor;
   };
 }
+// const converterDecorator = (sourceClass: NewableFunction, targetClass: NewableFunction) => {
+//   return function<T extends {new(...args:any[]):{}}>(ConverterConstructor : T){
+//     console.log(`Found converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
 
+//     const wrappedConverterConstructor: any = function (...args: any) {
+//         // console.log(`New: ${ConverterConstructor['name']} is created`);
+//         let converterInstance = new ConverterConstructor(...args);
+//         converterRegistry.register( <Converter<NewableFunction, NewableFunction>> converterInstance, sourceClass, targetClass);
+//         return converterInstance;
+//     }
+//     wrappedConverterConstructor.prototype = ConverterConstructor.prototype;
+
+//     return wrappedConverterConstructor;
+//   };
+// }
 
 const converterRegistry = new ConverterRegistry();
 export { converterRegistry };
