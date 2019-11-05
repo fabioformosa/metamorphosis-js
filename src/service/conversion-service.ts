6062be9680c48d4879fcd5de52fb21f8582c35bd
fabio.formosa <fabio.formosa@gmail.com>
2019-11-06 00:51:47 +0100

    minor commit

--- src/service/conversion-service.ts
+++ src/service/conversion-service.ts
@@ -13,14 +13,14 @@ export class ConversionService {
   convert(sourceObj: any, targetClass: NewableFunction): any{
     const converter = converterRegistry.getConverter(sourceObj.constructor, targetClass);
     if(!converter)
-      throw new Error(`Not found any converter to transform ${sourceObj.constructor.name} into ${targetClass.name}`);
+      throw new Error(`METAMORPHOSIS - Not found any converter to transform ${sourceObj.constructor.name} into ${targetClass.name}`);
     return converter.convert(sourceObj);
   }
   
   convertBySource(sourceObj: any, sourceClass: NewableFunction, targetClass: NewableFunction): any{
     const converter = converterRegistry.getConverter(sourceClass, targetClass);
     if(!converter)
-      throw new Error(`Not found any converter to transform ${sourceClass.name} into ${targetClass.name}`);
+      throw new Error(`METAMORPHOSIS - Not found any converter to transform ${sourceClass.name} into ${targetClass.name}`);
     return converter.convert(sourceObj);
   }
 
