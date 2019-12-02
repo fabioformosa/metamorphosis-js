e18cac52057816d4158820d743ee9bade0b641b7
Fabio Formosa <fabio.formosa@dedagroup.it>
2019-12-02 17:28:56 +0100

    minor fixes

--- test/custom-logger.spec.ts
+++ test/custom-logger.spec.ts
@@ -2,19 +2,19 @@ import CarToCarDtoConverter from "./service/car-to-cart-dto.converter";
 import Car from "./model/car";
 import CarDto from "./dto/car-dto";
 import Manufacturer from "./model/manufacturer";
-import { ConversionService } from "src/service/conversion-service";
+import { ConversionHelper} from "src/service/conversion-helper";
 
 describe('Test CarToCarDtoConverter', () => {
   const mockLogger = jest.fn();
 
-  let conversionService: ConversionService = new ConversionService({logger: mockLogger});
+  let conversionHelper: ConversionHelper = new ConversionHelper({logger: mockLogger});
   let carToCarDtoConverter: CarToCarDtoConverter = new CarToCarDtoConverter();
 
   it('test converter with conversion service', ()=> {
     const ferrari = new Manufacturer('Ferrari', 'Italy');
     const car = new Car('purosangue', 'red', ferrari);
 
-    const carDto: CarDto = <CarDto>(conversionService.convert(car, CarDto));
+    const carDto: CarDto = <CarDto>(conversionHelper.convert(car, CarDto));
     expect(carDto.color).toBe('red');
     expect(carDto.model).toBe('purosangue');
     expect(carDto.manufacturerName).toBe('Ferrari');
