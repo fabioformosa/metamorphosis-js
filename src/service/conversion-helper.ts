3c679d3df85549aa9bfd95787634ff9e08741096
Fabio Formosa <fabio.formosa@dedagroup.it>
2020-02-25 23:38:15 +0100

    #3 added plugin in conversionHelper constructor

--- src/service/conversion-helper.ts
+++ src/service/conversion-helper.ts
@@ -1,22 +1,24 @@
 import {converterRegistry} from '../decorators/converter-decorator';
 import { debugOpts, logger as loggerObj } from '../consts/log';
 import { pluginRegistry } from "../model/plugin-registry";
-
+import { MetamorphosisPlugin } from "../model/metamorphosis-plugin";
 
 
 export class ConversionHelper {
 
-  constructor({logger} : { logger: boolean | ((msg: string) => void)} = {logger: false}){
+  constructor({logger, plugins = []} :
+                { logger: boolean | ((msg: string) => void), plugins?: MetamorphosisPlugin[]} = {logger: false, plugins: []} ){
     debugOpts.enable = logger !== false;
-      if(debugOpts.enable && typeof logger === "function")
+    if(debugOpts.enable && typeof logger === "function")
       debugOpts.fn =  logger;
-      loggerObj.log(`METAMORPHOSIS - Created conversion service with debugOpts ${debugOpts.enable}`);
+    loggerObj.log(`METAMORPHOSIS - Created conversion service with debugOpts ${debugOpts.enable}`);
+    plugins.forEach(pluginRegistry.register);
   }
 
   convert(sourceObj: any, targetClass: NewableFunction): any{
     if(pluginRegistry.shouldRearrangeSourceType(sourceObj, targetClass)){
       const actualSourceType = pluginRegistry.rearrangeSourceType(sourceObj, targetClass);
-      loggerObj.log(`METAMORPHOSIS - Rearranged sourceType ${sourceObj.constructor} into ${actualSourceType}`);
+      loggerObj.log(`METAMORPHOSIS - Rearranged sourceType ${sourceObj.constructor.name} into ${actualSourceType}`);
       return this.convertBySource(sourceObj, actualSourceType, targetClass);
     }
     else
