2901b393c54aca9faa4faacb9227fde8b0ecb662
Fabio Formosa <fabio.formosa@dedagroup.it>
2021-01-03 15:20:06 +0100

    changed converter decorated to inherit the original prototype

--- src/decorators/converter-decorator.ts
+++ src/decorators/converter-decorator.ts
@@ -6,14 +6,30 @@ const converterDecorator = (sourceClass: NewableFunction, targetClass: NewableFu
   return function<T extends {new(...args:any[]):{}}>(ConverterConstructor : T){
     logger.log(`METAMORPHOSIS - Found converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
 
-    const wrappedConstructor : any = function (...args:any[]) {
-      const wrappedObj =  new ConverterConstructor(...args);
-      converterRegistrySingleton.register( <Converter<NewableFunction, NewableFunction>> <unknown>wrappedObj, sourceClass, targetClass);
-      logger.log(`METAMORPHOSIS - Registered new converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
-      return wrappedObj;
+    const wrappedConstructor : any = class extends ConverterConstructor{
+      constructor(...args: any[]){
+        super(...args);
+        converterRegistrySingleton.register( <Converter<NewableFunction, NewableFunction>> <unknown>this, sourceClass, targetClass);
+        logger.log(`METAMORPHOSIS - Registered new converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
+      }
     }
 
-    wrappedConstructor.prototype = ConverterConstructor.prototype;
+    // const wrappedConstructor : any = function (...args:any[]) {
+    //   const wrappedObj =  new ConverterConstructor(...args);
+    //   converterRegistrySingleton.register( <Converter<NewableFunction, NewableFunction>> <unknown>wrappedObj, sourceClass, targetClass);
+    //   logger.log(`METAMORPHOSIS - Registered new converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
+    //   return wrappedObj;
+    // }
+
+    // return class extends ConverterConstructor{
+    //   constructor(...args: any[]){
+    //     super(...args);
+    //     converterRegistrySingleton.register( <Converter<NewableFunction, NewableFunction>> <unknown>this, sourceClass, targetClass);
+    //     logger.log(`METAMORPHOSIS - Registered new converter ${ConverterConstructor.name} for ${sourceClass.name} to ${targetClass.name}`);
+    //   }
+    // }
+
+    // wrappedConstructor.prototype = ConverterConstructor.prototype;
     Object.defineProperty(wrappedConstructor, 'name', {value: ConverterConstructor.name, writable: false});
     return wrappedConstructor;
   };
